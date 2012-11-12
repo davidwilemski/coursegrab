ea6483e4fe7e37e300c2e5a4f9d028664d798457
David Wilemski <david@davidwilemski.com>
2012-11-12 17:33:24 -0500

    modify the way the schedule function works

--- geventutil.py
+++ geventutil.py
@@ -2,5 +2,5 @@
 import gevent
 
 def schedule(duration, func, *args, **kwargs):
-    func(*args, **kwargs)
-    gevent.spawn_later(duration, schedule, duration, func, *args, **kwargs).join()
+    gevent.spawn_later(duration, schedule, duration, func, *args, **kwargs)
+    gevent.spawn_later(0, func, *args, **kwargs)
